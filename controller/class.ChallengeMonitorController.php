Hackademic/hackademic

commit 82fe84a9b4da97722c0366468b1ffdb9a7dfeccb

Created an api for the challenges to interact with the backend
Each challenge-user pair has a unique id which allows the user to access the specific challenge
and identifies him for scoring.

Signed-off-by: Spyros Gasteratos <morfeas3000@gmail.com>

controller/class.ChallengeMonitorController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *
- * Hackademic-CMS/controller/class.UserMenuController.php
+ * Hackademic-CMS/controller/class.ChallengeMonitorController.php
  *
  * Hackademic User Menu Controller
  * Class for creating the frontend Main Menu
@@ -36,29 +36,96 @@ require_once(HACKADEMIC_PATH."model/common/class.Session.php");
 require_once(HACKADEMIC_PATH."model/common/class.ChallengeAttempts.php");
 require_once(HACKADEMIC_PATH."admin/model/class.ClassMemberships.php");
 require_once(HACKADEMIC_PATH."admin/model/class.ClassChallenges.php");
+require_once(HACKADEMIC_PATH."model/common/class.UserHasChallengeToken.php");
 
 class ChallengeMonitorController {
     
     public function go() {
         // Check Permissions
     }
-    
-    public function update($status) {
-        if (!Session::isAdmin() && !Session::isTeacher()) {
-            $username = Session::getLoggedInUser();
-            $url = $_SERVER['REQUEST_URI'];
-            $url_components = explode("/", $url);
-            $count_url_components = count($url_components);
-            for ($i=0; $url_components[$i] != "challenges"; $i++);
-            $pkg_name = $url_components[$i+1];
-            $user = User::findByUserName($username);
-            $challenge = Challenge::getChallengeByPkgName($pkg_name);
-            $user_id = $user->id;
-            $challenge_id = $challenge[0]->id;
-            if (!ChallengeAttempts::isChallengeCleared($user_id, $challenge_id)) {
-                ChallengeAttempts::addChallengeAttempt($user_id, $challenge_id, $status);
-            }
-        }
-    }
+    public function start($userid=null, $chid=null, $token=null){
+		if(!isset($_SESSION))
+			session_start();
+			
+		//echo"<p>";var_dump($token);echo "</p>";
+		//echo"<p>";var_dump($_SESSION['token']);echo "</p>";
+		
+		if(!isset($_SESSION['chid']))
+			$_SESSION['chid'] = $chid;
+		if(!isset($_SESSION['token']))
+			$_SESSION['token'] = $token;
+		if(!isset($_SESSION['userid']))
+			$_SESSION['userid'] = $userid;
+		//echo"<p>";var_dump($_SESSION['token']);echo "</p>";
+
+		$url = $_SERVER['REQUEST_URI'];
+        $url_components = explode("/", $url);
+        $count_url_components = count($url_components);
+        for ($i=0; $url_components[$i] != "challenges"; $i++);
+		$pkg_name = $url_components[$i+1];
+		
+		if(!isset($_SESSION['pkg_name']))
+			$_SESSION['pkg_name'] = $pkg_name;
+			
+		$pair = UserHasChallengeToken::findByPair($userid,$chid,$token);
+		
+		/*If token is the one in the session then challenge must be the same*/
+		if($_SESSION['token'] == $token)
+		if($pkg_name != $_SESSION['pkg_name']  || $_SESSION['chid'] != $chid){
+			error_log("HACKADEMIC::ChallengeMonitorController::RIGHT token WRONG CHALLENGE ".$_SESSION['pkg_name']);
+			header("Location: ".SITE_ROOT_PATH);
+		}
+		/* If token changed AND the challenge changed AND its a valid token for that challenge then we are in a new challenge*/
+		if($_SESSION['token'] != $token && $token!=null)
+			if($pkg_name != $_SESSION['pkg_name']  || $_SESSION['chid'] != $chid){
+				if($pair[0]->token == $token){
+					$_SESSION['chid'] = $chid;
+					$_SESSION['token'] = $token;
+					$_SESSION['pkg_name'] = $pkg_name;
+				}
+			}else{
+				error_log("HACKADEMIC::ChallengeMonitorController::WRONG CHALLENGE ".$_SESSION['pkg_name']);
+				header("Location: ".SITE_ROOT_PATH);}
+		
+		/*echo"<p>";var_dump($pair);echo "</p>";
+		echo"<p>";var_dump($token);echo "</p>";
+		echo"<p>";var_dump($_SESSION['token']);echo "</p>";
+		*/
+		if($pair[0]->token != $token){
+			error_log("HACKADEMIC::ChallengeMonitorController::pair->token != $token",$pair[0]->token);
+			header("Location: ".SITE_ROOT_PATH);
+		
+		}
+	}
+    public function update($status,$userid = null ,$chid = null ,$token = null) {
+		
+		$this->start($userid,$chid,$token);
+		/*IF status == init we only need to update the SESSION var*/
+		if($status == CHALLENGE_INIT)
+		return;
+
+		if($userid == null)
+			$userid = $_SESSION['userid'];
+		if($chid == null)
+			$chid = $_SESSION['chid'];
+		if($token == null)
+			$token = $_SESSION['token'];
+
+        $username = $userid;
+        $url = $_SERVER['REQUEST_URI'];
+        $url_components = explode("/", $url);
+        $count_url_components = count($url_components);
+        for ($i=0; $url_components[$i] != "challenges"; $i++);
+		$pkg_name = $url_components[$i+1];
+        $user = User::findByUserName($username);
+        $challenge = Challenge::getChallengeByPkgName($pkg_name);
+        if($user)
+           $user_id = $user->id;
+         $challenge_id = $challenge[0]->id;
+         if (!ChallengeAttempts::isChallengeCleared($user_id, $challenge_id)) {
+			ChallengeAttempts::addChallengeAttempt($user_id, $challenge_id, $status);
+          }
+   }
+ 
     
 }

controller/class.TryChallengeController.php
@@ -28,13 +28,14 @@
  * @author Konstantinos Papapanagiotou <conpap[at]gmail[dot]com>
  * @license http://www.gnu.org/licenses/gpl.html
  * @copyright 2012 OWASP
- *
+ *UserHasChallengeToken
  */
 require_once(HACKADEMIC_PATH."model/common/class.Challenge.php");
 require_once(HACKADEMIC_PATH."model/common/class.User.php");
 require_once(HACKADEMIC_PATH."admin/model/class.ClassMemberships.php");
 require_once(HACKADEMIC_PATH."admin/model/class.ClassChallenges.php");
 require_once(HACKADEMIC_PATH."admin/controller/class.HackademicBackendController.php");
+require_once(HACKADEMIC_PATH."model/common/class.UserHasChallengeToken.php");
 
 class TryChallengeController extends HackademicController {
 
@@ -44,21 +45,30 @@ class TryChallengeController extends HackademicController {
 		    $this->addToView('id', $id);
 		    $challenge=Challenge::getChallenge($id);
 		    if ($this->isLoggedIn() && ($this->isAdmin() || self::IsAllowed($this->getLoggedInUser(), $challenge[0]->id))) {
-			$challenge_path = SOURCE_ROOT_PATH."challenges/".$challenge[0]->pkg_name."/";
-			$this->addToView('pkg_name', $challenge[0]->pkg_name);
-			$solution = $challenge[0]->solution;
-			if (isset($_POST) && count($_POST)!=0) {
-				//echo '<div style = "color:red">CHALLENGE WAS SUBMITTED</div>';
+				$challenge_path = SOURCE_ROOT_PATH."challenges/".$challenge[0]->pkg_name."/";
+				$this->addToView('pkg_name', $challenge[0]->pkg_name);
+				$solution = $challenge[0]->solution;
+				if (isset($_POST) && count($_POST)!=0) {
+					//echo '<div style = "color:red">CHALLENGE WAS SUBMITTED</div>';
+				}
+				if (!isset($_GET["path"])) {
+					$url = $challenge_path."index.php";
+				}else {
+					$url = $challenge_path.$_GET['path'];
+				}
+				if(isset($_GET['user']) && $_GET['user'] == $this->getLoggedInUser()){
+					$usr = $this->getLoggedInUser();
+					$url.='?user='.$usr."&id=".$id;
+					Global $ESAPI_utils;
+					$token = $ESAPI_utils->getRandomizer()->getRandomGUID();
+					$token = $ESAPI_utils->getEncoder()->encodeForURL($token);
+					$url.='&token='.$token;
+					UserHasChallengeToken::add($usr,$id,$token);
+				}
+				header("Location: ".$url);
+		    }else {
+				header("Location: ".SITE_ROOT_PATH);
 			}
-			if (!isset($_GET["path"])) {
-				$url = $challenge_path."index.php";
-			} else {
-				$url = $challenge_path.$_GET['path'];
-			}
-			header("Location: ".$url);
-		    } else {
-			die();
-		    }
 		}
 		$this->setViewTemplate("trychallenge.tpl");
 		$this->generateView();

model/common/class.UserHasChallengeToken.php
@@ -1,0 +1,98 @@
+<?php
+/**
+ *
+ * Hackademic-CMS/model/common/class.User.php
+ *
+ * Hackademic User Model
+ * Class for Hackademic's User Object
+ * 
+ * Copyright (c) 2012 OWASP
+ *
+ * LICENSE:
+ *
+ * This file is part of Hackademic CMS (https://www.owasp.org/index.php/OWASP_Hackademic_Challenges_Project).
+ *
+ * Hackademic CMS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Hackademic CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with Hackademic CMS.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @author Pragya Gupta <pragya18nsit[at]gmail[dot]com>
+ * @author Konstantinos Papapanagiotou <conpap[at]gmail[dot]com>
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2012 OWASP
+ *
+ */
+require_once(HACKADEMIC_PATH."model/common/class.HackademicDB.php");
+require_once(HACKADEMIC_PATH."model/common/class.Utils.php");
+
+class UserHasChallengeToken {
+	public $id;
+	public $user_id;
+	public $token;
+	public $challenge_id;
+
+	public static function add($userid=null, $chid=null, $token=null) {
+		global $db;
+		$params = array(
+				':user_id' => $userid,
+				':challenge_id' => $chid,
+				':token' => $token
+			     );
+		$exists = self::findByPair($userid,$chid);
+		if($exists != NULL){
+			$sql =  'UPDATE  user_has_challenge_token SET token = :token
+					 WHERE user_id = :user_id AND challenge_id = :challenge_id';
+		}else{
+			$sql = 'INSERT INTO user_has_challenge_token(user_id,challenge_id,token) VALUES(
+					:user_id,:challenge_id,:token)';
+		}
+		$query = $db->query($sql, $params); 
+		if ($db->affectedRows($query)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static function findByPair($userid,$chid){
+		global $db;
+		$sql = "SELECT * FROM user_has_challenge_token WHERE
+				user_id = :user_id AND challenge_id = :challenge_id
+				LIMIT 1";
+		$params = array(
+				':user_id' => $userid,
+				':challenge_id' => $chid
+			     );
+		$object_array=self::findBySQL($sql, $params);
+		return $object_array;
+	}  
+	private static function findBySQL($sql, $params = NULL) {
+		global $db;
+		$result_set=$db->query($sql, $params);
+		$object_array=array();
+		while($row=$db->fetchArray($result_set)) {
+			$object_array[]=self::instantiate($row);
+		}
+		return $object_array;
+	}
+	public static function instantiate($record) {
+		$object=new self;
+		foreach($record as $attribute=>$value) {
+			if($object->hasAttribute($attribute)) {
+				$object->$attribute=$value;
+			}
+		}
+		return $object;
+	}
+	private function hasAttribute($attribute) {
+		$object_vars=get_object_vars($this);
+		return array_key_exists($attribute,$object_vars);
+	}
+}

admin/model/class.ClassChallenges.php
@@ -93,7 +93,7 @@ class ClassChallenges {
 			$result_array[$i]["title"] = $row['title'];
 			$i++;
 		}
-		Debug::show($result_array,'all',$this,_FUNCTION_);
+		//Debug::show($result_array,'all',$this,_FUNCTION_);
 		return $result_array;
 	}
 

challenges/ch001/main/index.php
@@ -10,8 +10,12 @@
  */
 ?>
 <?php
-session_start();
-require_once($_SESSION['hackademic_path']."pages/challenge_monitor.php");
+
+		include_once dirname(__FILE__).'/../../../init.php';		
+        session_start();
+        require_once(HACKADEMIC_PATH."pages/challenge_monitor.php");
+         $monitor->update(CHALLENGE_INIT,$_GET['user'],$_GET['id'],$_GET['token']);
+
 $name1 = $_POST["name1"];
 $name2 = $_POST["name2"];
 

challenges/ch002/index.php
@@ -12,8 +12,11 @@
  
 ?>
 <?php
+		include_once dirname(__FILE__).'/../../init.php';		
         session_start();
-        require_once($_SESSION['hackademic_path']."pages/challenge_monitor.php");
+        require_once(HACKADEMIC_PATH."pages/challenge_monitor.php");
+        $monitor->update(CHALLENGE_INIT,$_GET['user'],$_GET['id'],$_GET['token']);
+ 
 	$result =  $_GET['Result'];
 	if ($result === 'enter a coin to play'){
 		echo "<h1><br><center>Congratulations!</br></cetner></h1>";

challenges/ch003/index.php
@@ -24,18 +24,20 @@
 <h2>
 <hr>
 <?php
-session_start();
-require_once($_SESSION['hackademic_path']."pages/challenge_monitor.php");
+		include_once dirname(__FILE__).'/../../init.php';		
+        session_start();
+        require_once(HACKADEMIC_PATH."pages/challenge_monitor.php");
+        
 	$try_xss = $_POST['try_xss'];
 	if  ( ($try_xss == '<script>alert("XSS!");</script>') OR
 		  ($try_xss == "<script>alert('XSS!');</script>") ) {
     		echo 'Thank you'.' '.($_POST['try_xss']).'!';
 			echo "<H1>Congratulations!</H1>";
-			$monitor->update(CHALLENGE_SUCCESS);
+			$monitor->update(CHALLENGE_SUCCESS,$_GET['user'],$_GET['id'],$_GET['token']);
 	
     } 
 	else {
-		$monitor->update(CHALLENGE_FAILURE);
+		$monitor->update(CHALLENGE_FAILURE,$_GET['user'],$_GET['id'],$_GET['token']);
 ?>
 	Try to XSS me using the straight forward way... <br />
 	<form method="POST">

challenges/ch004/index.php
@@ -25,18 +25,19 @@
 <h2>
 <hr>
 <?php
-	session_start();
-        require_once($_SESSION['hackademic_path']."pages/challenge_monitor.php");
-	// <script>alert(String.fromCharCode(88,88,83,33))</script>
+		include_once dirname(__FILE__).'/../../init.php';		
+        session_start();
+        require_once(HACKADEMIC_PATH."pages/challenge_monitor.php");
+        // <script>alert(String.fromCharCode(88,88,83,33))</script>
 	$try_xss = $_POST['try_xss'];
 	if($try_xss == '<script>alert(String.fromCharCode(88,83,83,33))</script>') {
     		echo 'Thank you '.$try_xss.'';
 			echo "<H1>Congratulations!</H1>";
-			$monitor->update(CHALLENGE_SUCCESS);
+			$monitor->update(CHALLENGE_SUCCESS,$_GET['user'],$_GET['id'],$_GET['token']);
     		
     } 
 	else {
-		$monitor->update(CHALLENGE_FAILURE);
+		$monitor->update(CHALLENGE_FAILURE,$_GET['user'],$_GET['id'],$_GET['token']);
 ?>
 	Try to XSS me...Again! <br />
 	<form method="POST">

commit 1cce2062a6ab05a552eef9f1dfdac6adc7e25593

fixed bug introduced by commit 82fe84a9b4da97722c0366468b1ffdb9a7dfeccb
Signed-off-by: Spyros Gasteratos <morfeas3000@gmail.com>

controller/class.ChallengeMonitorController.php
@@ -39,17 +39,17 @@ require_once(HACKADEMIC_PATH."admin/model/class.ClassChallenges.php");
 require_once(HACKADEMIC_PATH."model/common/class.UserHasChallengeToken.php");
 
 class ChallengeMonitorController {
-    
+
     public function go() {
         // Check Permissions
     }
     public function start($userid=null, $chid=null, $token=null){
 		if(!isset($_SESSION))
 			session_start();
-			
+
 		//echo"<p>";var_dump($token);echo "</p>";
 		//echo"<p>";var_dump($_SESSION['token']);echo "</p>";
-		
+
 		if(!isset($_SESSION['chid']))
 			$_SESSION['chid'] = $chid;
 		if(!isset($_SESSION['token']))
@@ -63,12 +63,12 @@ class ChallengeMonitorController {
         $count_url_components = count($url_components);
         for ($i=0; $url_components[$i] != "challenges"; $i++);
 		$pkg_name = $url_components[$i+1];
-		
+
 		if(!isset($_SESSION['pkg_name']))
 			$_SESSION['pkg_name'] = $pkg_name;
-			
+
 		$pair = UserHasChallengeToken::findByPair($userid,$chid,$token);
-		
+
 		/*If token is the one in the session then challenge must be the same*/
 		if($_SESSION['token'] == $token)
 		if($pkg_name != $_SESSION['pkg_name']  || $_SESSION['chid'] != $chid){
@@ -78,27 +78,30 @@ class ChallengeMonitorController {
 		/* If token changed AND the challenge changed AND its a valid token for that challenge then we are in a new challenge*/
 		if($_SESSION['token'] != $token && $token!=null)
 			if($pkg_name != $_SESSION['pkg_name']  || $_SESSION['chid'] != $chid){
-				if($pair[0]->token == $token){
+				//var_dump($pair);die();
+				if($pair->token == $token){
 					$_SESSION['chid'] = $chid;
 					$_SESSION['token'] = $token;
 					$_SESSION['pkg_name'] = $pkg_name;
 				}
 			}else{
+				//var_dump($token);die();
 				error_log("HACKADEMIC::ChallengeMonitorController::WRONG CHALLENGE ".$_SESSION['pkg_name']);
-				header("Location: ".SITE_ROOT_PATH);}
-		
+				header("Location: ".SITE_ROOT_PATH);
+			}
+
 		/*echo"<p>";var_dump($pair);echo "</p>";
 		echo"<p>";var_dump($token);echo "</p>";
 		echo"<p>";var_dump($_SESSION['token']);echo "</p>";
 		*/
-		if($pair[0]->token != $token){
-			error_log("HACKADEMIC::ChallengeMonitorController::pair->token != $token",$pair[0]->token);
+		if($pair->token != $token){
+			error_log("HACKADEMIC::ChallengeMonitorController::pair->token != $token".$pair[0]->token);
 			header("Location: ".SITE_ROOT_PATH);
-		
+
 		}
 	}
     public function update($status,$userid = null ,$chid = null ,$token = null) {
-		
+
 		$this->start($userid,$chid,$token);
 		/*IF status == init we only need to update the SESSION var*/
 		if($status == CHALLENGE_INIT)
@@ -126,6 +129,6 @@ class ChallengeMonitorController {
 			ChallengeAttempts::addChallengeAttempt($user_id, $challenge_id, $status);
           }
    }
- 
-    
+
+
 }

controller/class.TryChallengeController.php
@@ -59,21 +59,29 @@ class TryChallengeController extends HackademicController {
 				if(isset($_GET['user']) && $_GET['user'] == $this->getLoggedInUser()){
 					$usr = $this->getLoggedInUser();
 					$url.='?user='.$usr."&id=".$id;
-					Global $ESAPI_utils;
-					$token = $ESAPI_utils->getRandomizer()->getRandomGUID();
-					$token = $ESAPI_utils->getEncoder()->encodeForURL($token);
-					$url.='&token='.$token;
-					UserHasChallengeToken::add($usr,$id,$token);
+					$pair = UserHasChallengeToken::findByPair($usr,$id);
+					if($pair === false){
+						error_log("adding new token usr, id".$usr." ".$id);
+						Global $ESAPI_utils;
+						$token = $ESAPI_utils->getRandomizer()->getRandomGUID();
+						$token = $ESAPI_utils->getEncoder()->encodeForURL($token);
+						UserHasChallengeToken::add($usr,$id,$token);
+						$pair = new UserHasChallengeToken();
+						$pair->token = $token;
+					}
+					$url.='&token='.$pair->token;
+					var_dump($pair);
 				}
 				header("Location: ".$url);
 		    }else {
+				error_log("dafuq?");
 				header("Location: ".SITE_ROOT_PATH);
 			}
 		}
 		$this->setViewTemplate("trychallenge.tpl");
 		$this->generateView();
 	}
-	
+
 	protected static function isAllowed($username, $challenge_id) {
 		$user = User::findByUserName($username);
 		$dbg_array = ClassChallenges::getChallengesOfUser($user->id);

model/common/class.UserHasChallengeToken.php
@@ -5,7 +5,7 @@
  *
  * Hackademic User Model
  * Class for Hackademic's User Object
- * 
+ *
  * Copyright (c) 2012 OWASP
  *
  * LICENSE:
@@ -54,7 +54,7 @@ class UserHasChallengeToken {
 			$sql = 'INSERT INTO user_has_challenge_token(user_id,challenge_id,token) VALUES(
 					:user_id,:challenge_id,:token)';
 		}
-		$query = $db->query($sql, $params); 
+		$query = $db->query($sql, $params);
 		if ($db->affectedRows($query)) {
 			return true;
 		} else {
@@ -64,15 +64,18 @@ class UserHasChallengeToken {
 	public static function findByPair($userid,$chid){
 		global $db;
 		$sql = "SELECT * FROM user_has_challenge_token WHERE
-				user_id = :user_id AND challenge_id = :challenge_id
+				user_id= :user_id AND challenge_id= :challenge_id
 				LIMIT 1";
 		$params = array(
 				':user_id' => $userid,
 				':challenge_id' => $chid
 			     );
 		$object_array=self::findBySQL($sql, $params);
-		return $object_array;
-	}  
+		/*var_dump($userid);
+		var_dump($chid);
+		var_dump($object_array);die();*/
+		return !empty($object_array)?array_shift($object_array):false;
+	}
 	private static function findBySQL($sql, $params = NULL) {
 		global $db;
 		$result_set=$db->query($sql, $params);