a6a626c22843e14c2f15610474ee025cff50237b
Daniel Kvist <daniel@danielkvist.net>
2013-07-15 12:18:24 +0200

    Fix #2 #3 implement custom plugin and theme

---
+++ user/themes/custom-theme/custom-theme.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ * Plugin Name: Custom theme
+ * Plugin URI: http://example.com/article-challenge-connect
+ * Description: A brief description of the theme.
+ * Version: 1.0
+ * Author: Daniel Kvist
+ * Author URI: http://danielkvist.net
+ * License: GPL2
+ */
