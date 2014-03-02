aed7c99544a7b96fd60321b3800267d9bf94c548
Paul Chaignon <paul.chaignon@gmail.com>
2014-03-02 20:08:53 +0100

    Corrections to the installation state menu on the right

--- installation/installer/Installer.php
+++ installation/installer/Installer.php
@@ -82,7 +82,7 @@ class Installer
 			$this->view->error($this->lang['L-01']);
 		}
 
-		$allwed_steps = array('admin'=>'adminAction', 'index' => 'indexAction', 'db' => 'dbAction', 'cfg' => 'configAction', 'database' => 'dbTables', 'config' => 'configWrite', 'finish' => 'finishInstaller');
+		$allwed_steps = array('admin'=>'adminAction', 'index' => 'indexAction', 'db' => 'dbAction', 'cfg' => 'configAction', 'dbdone' => 'dbTables', 'config' => 'configWrite', 'finish' => 'finishInstaller');
 		if(!in_array($_POST['step'], array_keys($allwed_steps)))
 		{
 			$this->nextStep('index');
