6b589897268dc0beddc60a1bb72c328789954181
MisterIcy <icyd3mon@gmail.com>
2014-04-17 12:06:49 +0300

    Comments translation for Izon Challenge

--- challenges/izon/login.php
+++ challenges/izon/login.php
@@ -1,8 +1,7 @@
 <?php
-/* κάνουμε include το αρχείο "config.inc.php" του hackademic
-ουτως ώστε να έχουμε πρόσβαση στα definition DB_HOST, DB_NAME κλπ τα οποία 
-μας δίνουν την δυνατότητα να συνδεθούμε στον MySQL server και ξεκινάμε και το session 
-για να έχουμε πρόσβαση στις μεταβλητές του*/
+/* init.php from hackademic allows us to use its functions while
+   config.inc.php contains the credentials for the temporary mysql
+   connection that allows the injections to be done */
 session_start();
 include_once dirname(__FILE__).'/../../init.php';		
 include_once 'config.inc.php';
@@ -10,42 +9,37 @@ include_once 'config.inc.php';
 
 class User {
 
-	/* Δημόσιες μεταβλητές username και password οι οποίες χρησιμοποιούνται για να κάνουμε
-	validate ένα χρήστη που προσπαθεί να συνδεθεί στο site */
+	/* Public variables of the class, meant to store username and password 
+	   in order to login */
 	public $username;
 	public $password;
 	
 	private $database_name;
-	/* Ο κονστράκτοράς μας */
+	/* Our constructor */
 	function __construct() {
-		session_start();
-		/* Ανοίγουμε μια νέα σύνδεση στον SQL Server με τα στοιχεία που πήραμε απο το
-		αρχείο config.inc.php. 
-		ΣΗΜΑΝΤΙΚΟ!!!1 (ενα θαυμαστικό): Ο χρήστης πρέπει να έχει δικαιώματα για δημιουργία 
-		νέας βάσης δεδομένων και δικαιώματα για να σβήνει βάσεις δεδομένων*/
+		/* A new connection is created to MySQL in order to create the temporary user/database/table
+		   used in the injection 
+		   IMPORTANT: The user must be able to CREATE/DROP database, CREATE/DROP user (etc, see readme) */
 		$db_link = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 		
-		/* Βάσει της τιμής του cookie του hackademic, παίρνουμε ένα string (για την ακρίβεια παίρνουμε
-		το crc32 της τιμής του) το οποίο θα χρησιμοποιηθεί ώς όνομα βάσης δεδομένων και όνομα του χρήστη στην SQL */
+		/* We calculate the CRC32 value of PHPSESSID cookie (this is the unique identifier php assigns to each
+		   user's session) in order to use it as database/database username/username in izon challenge */
 		$this->database_name = hash('crc32', $_COOKIE['PHPSESSID']);
 		
-		/* Φτιάχνουμε μια νέα βάση δεδομένων για να μπορέσει να δουλέψει εκεί */
+		/* We create a new database */
 		$db_link->query("CREATE DATABASE hack_" . $this->database_name);
 		
-		/* Φτιάχνουμε ένα νέο χρήστη στην mysql ο οποίος θα μπορεί να συνδεθεί στην βάση δεδομένων (για να μην 
-		γίνει privilege abuse και να μην υπάρξει κενό ασφαλείας) */
+		/* And then create a new user*/
 		$db_link->query("CREATE USER 'hack_" . $this->database_name  ."'@'" . DB_HOST . "' IDENTIFIED BY 'hackademic_password'");
 		
-		/* Αφαιρούμε όλα τα privileges απο τον χρήστη που φτιάξαμε και την επιλογή GRANT (με την οποία μπορει 
-		να δώσει privileges στον εαυτό του ή σε κάποιον άλλο χρήστη) */
+		/* Remove all privileges from the new user (and GRANT OPTION) so that he cannot mess with the database */
 		$db_link->query("REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'hack_" . $this->database_name  ."'@'" . DB_HOST . "'");
 				
-		/* Και τέλος του δίνουμε privileges μόνο στην νέα βάση δεδομένων που έχουμε δημιουργήσει */
+		/* Finally, we grant him privileges only on our database */
 		$db_link->query("GRANT ALL ON hack_" . $this->database_name .".* TO 'hack_" . $this->database_name  ."'@'" . DB_HOST . "'");
 		$db_link->query("FLUSH PRIVILEGES");
 		
-		/* Δημιουργούμε την δομή του πίνακα users 
-		Βήμα πρώτο, δημιουργούμε το query της δομή του*/
+		/* Table creation step one: Structure */
 		$table = "CREATE TABLE IF NOT EXISTS `users` (
 		`id` int(11) NOT NULL AUTO_INCREMENT,
 		`username` varchar(255) NOT NULL,
@@ -61,127 +55,117 @@ class User {
 		UNIQUE KEY `id` (`id`)
 		) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1";
 	
-		/* Βήμα Δεύτερο: Επιλέγουμε την βάση που δημιουργήσαμε */
+		/* Step Two: Selection of the newly created db */
 		$db_link->select_db("hack_" . $this->database_name);
 		
-		/* Και τέλος, δημιουργούμε τον πίνακα κάνοντας query */
+		/* Step Three: Creation of the table*/
 		$db_link->query($table);
 		
-		/* Έπειτα, δημιουργούμε δύο εγγραφές, μία για τον χρήστη admin και μία για τον απλό χρήστη */
+		/* Then, we insert two records, one for the admin user and another for the regular user */
 		$sql = 'INSERT INTO `users` (`id`, `username`, `full_name`, `email`, `password`, `joined`, `last_visit`, `is_activated`, `type`, `token`) VALUES (NULL, \'admin\', \'kapoios\', \'kati@kapou.com\', \'enadiotria\', \'2014-01-15 00:00:00\', \'2014-01-23 00:00:00\', \'1\', \'1\', \'1\')';
 		$db_link->query($sql);
 		$sql = 'INSERT INTO `users` (`id`, `username`, `full_name`, `email`, `password`, `joined`, `last_visit`, `is_activated`, `type`, `token`) VALUES (NULL, \''.$this->database_name . '\', \'kapoios\', \'kati@kapou.com\', \''.hexdec($this->database_name).'\', \'2014-01-15 00:00:00\', \'2014-01-23 00:00:00\', \'1\', \'1\', \'1\')';
 		$db_link->query($sql);
 		
-		/* και κλείνουμε το connection */
+		/* Finally we close the connection */
 		$db_link->close();
 		
 	}
-	
+	/* Our Destructor */
 	function __destruct() {
-		/* Ανοίγουμε μια νέα σύνδεση στον SQL Server με τα στοιχεία που πήραμε απο το
-		αρχείο config.inc.php. 
-		ΣΗΜΑΝΤΙΚΟ!!!1 (ενα θαυμαστικό): Ο χρήστης πρέπει να έχει δικαιώματα για δημιουργία 
-		νέας βάσης δεδομένων και δικαιώματα για να σβήνει βάσεις δεδομένων*/
+		/* As in constructor, open a new connection with a user that has full privileges */
 		$db_link = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 		
-		/* Διαγράφουμε την προσωρινή βάση δεδομένων */
+		/* Drop our database */
 		$db_link->query("DROP DATABASE hack_" . $this->database_name);
 		
-		/* Διαγράφουμε τον χρήστη που έχουμε δημιουργήσει */
+		/* Delete the user we've created */
 		$db_link->query("DROP USER 'hack_" . $this->database_name  ."'@'" . DB_HOST . "'");
-		//χρειαζόμασε κι ένα FLUSH γιατι κάποιες φορές υπάρχει πρόβλημα λόγω του: http://bugs.mysql.com/bug.php?id=28331
+		/* A FLUSH PRIVILEGES is needed, because sometimes problems arise due to: http://bugs.mysql.com/bug.php?id=28331 */
 		$db_link->query("FLUSH PRIVILEGES");
-		/* Και τέλος, κλείνουμε το connection */
 		
+		/* Finally, close the connection */
 		$db_link->close();
 	}
 	public function login() {
-		/* Κατ' αρχάς ελέγχουμε αν ο χρήστης έχει συμπληρώσει τα πεδία username και password της φόρμας */
+		/* We check if the user send the POST Data correctly */
 		if ((!isset($_POST['username'])) || (!isset($_POST['password'])) ) {
 			$this->sendPostData(1);
 		}
-		/* Κατα δεύτερον ελέγχουμε οτι τα πεδία αυτά δεν έιναι κενά */
+		/* Then, check if he has given values to username/password */
 		if ((($_POST['username'])=="") || (($_POST['password']) == "")) {
 			$this->sendPostData(2);
 		}
-		if (!isset($_SESSION['ch01'])) {	//Για να μην γίνεται πολλές φορές το Header Forgery 
-			/* Κατά τρίτον, ελέγχουμε για τον σωστό referer (Challenge 1: HTTP Header Forgery) */
+		/* If the first challenge is complete, we skip this check in order to simplify the wargame */
+		if (!isset($_SESSION['ch01'])) {
+			/* First part of the header forgery challenge: check for the correct referrer */
 			if ($_SERVER['HTTP_REFERER'] != "http://www.izon.com/") {
 				$this->sendPostData(3);
 			}
-			/* Κατα τέταρτον, ελέχγουμε για τον σωστό browser (Challenge 1: HTTP Header Forgery) */
+			/* Second part of the header forgery challenge: check for the correct user-agent */ 
 			if ($_SERVER['HTTP_USER_AGENT'] != "Izon Browser" ) {
 				$this->sendPostData(4);
 			}
 		}
-		/* Δημιουργούμε μια νέα σύνδεση στην sql, με τα στοιχεία του χρήστη που έχουμε φτιάξει έτσι ώστε
-		να έχουμε πρόσβαση στην προσωρινή βάση δεδομένων */
+		/* Open a new connection with the credentials of the temporary user we created in our constructor */
 		$db_link = new mysqli(DB_HOST, "hack_" . $this->database_name, 'hackademic_password', "hack_" . $this->database_name);
 		
-		/* Βάζουμε τα user/pass στις μεταβλητές της κλάσης */
+		/* Assign values to class's variables */
 		$this->username = $_POST['username'];
 		$this->password = $_POST['password'];
-		/* Κάνουμε ένα query στην βάση δεδομένων για να δούμε άν όντως υπάρχει το συγκεκριμένο username/password στην βάση δεδομένων
-		Ακριβώς εδώ μπορεί να γίνει sql injection.*/
+		/* And do the query to find our user. Injection happens here */
 		$our_query = "SELECT * FROM users WHERE	username='" . $this->username . "' AND password='" . $this->password . "'";
 		$result = $db_link->query($our_query);
 		
-		/* Εαν η sql μας επιστρέψει περισσότερες απο μηδεν εγγραφές (αυτό γίνεται επίτηδες για να καταλάβουμε ότι ο χρήστης μας έκανε
-		sql injection */
-		
+		/* If the result has rows, then we either have a successful login or a successful injection */
 		if ($result->num_rows > 0) {
-			/* Λέμε στην sql να μας επιστρέψει μόνο την πρώτη γραμμή για να δούμε ποιο είναι το username */
+			/* We only need to fetch the first row in order to check the username */
 			$row = $result->fetch_row();
-			/* Και την εκχωρούμε στην μεταβλητή username της κλάσης */
+			/* and assign it to username variable (it uses $row[1] because username field is the second field of the table) */
 			$this->username = $row[1];
-			/* Αν ο χρήστης έχει κάνει login ως admin (successful sql injection)
-			αλλά δεν έχει περάσει το πρώτο challenge, τότε πρέπει να τον γυρίσουμε πίσω και να του
-			πούμε πως δεν μπορεί να το κάνει ακόμη αυτό */
+			/* If username is admin (successful sql injection), but hasn't gone through the first challenge, 
+			   we must send him back, telling him that he cannot do that yet */
 			if ($this->username == 'admin') {
-				/* αυτό επιτυγχάνεται με τον έλεγχο της μεταβλητής $_SESSION['ch01']
-				που κανονικά παίρνει τιμή λίγο πιο κάτω*/
+				/* This is possible by checking session variable ch01 */
 				if (!isset($_SESSION['ch01'])) {
 					$this->sendPostData(7);
 				} else {
 					$_SESSION['ch02']=1;
-					/* Αν το timer δεν έχει τιμή του δίνουμε ( η άλλη περίπτωση είναι να ξανακάνει login ο χρήστης άρα και να 
-					ξαναλύσει το challenge ) */
+					/* If all went well, we set the challenge timer in order to display user's progress */
 					if (!isset($_SESSION['ch02_timer'])) { $_SESSION['ch02_timer']=time(); }
 				}
+			/* The other case is when the user tries to login as a regular user with the give username */
 			} else if ($this->username = hash('crc32', $_COOKIE['PHPSESSID'])) {
 				$_SESSION['ch01'] = 1;
-				/* Αν το timer δεν έχει τιμή του δίνουμε ( η άλλη περίπτωση είναι να ξανακάνει login ο χρήστης άρα και να 
-				ξαναλύσει το challenge ) */
+				/* we set the challenge timer in order to display user's progress */
 				if (!isset($_SESSION['ch01_timer'])) { $_SESSION['ch01_timer']=time(); }
-				/* Μάλλον ο χρήστης μας ήταν πολύ μπροστά κι έκανε injection και στο πρώτο challenge!!! */
+				/* If the user didn't use the supplied password, he injected his way in the first challenge, give him the easter egg*/
 				if ($this->password != hexdec($this->username)) {
 					$_SESSION['ch01_egg'] = 1;
 				}
 			}
-			/* Έπειτα φτιάχνουμε το cookie για να μείνει logged in ο χρήστης μας */
+			/* Create the cookie in order to have him logged in */
 			$cook = setcookie('izon',  $this->username, time()+3600);
-			/* Αν όλα πήγαν καλά, ανακατευθύνουμε τον χρήστη στο index */
+			/* If all went well, redirect him to index */
 			if ($cook) {
 				header("Location: index.php");
+				die();
 			} else {
-			/* Πες στον χρήστη οτι υπήρξε πρόβλημα κατα την δημιουργία του cookie */
+				/* Tell him that there was an error, while baking the cookie */
 				$this->sendPostData(6);
 			}
 		} else {
-			/* δεν βρέθηκε κανένας χρήστης, οπότε ενημέρωσε τον χρήστη μας πως το username/password combination δεν είναι εγκυρο */
+			/* No user found! */
 			$this->sendPostData(5);
 		}
 	}
-	/* βοηθητική function που χρησιμοποιείται για να κάνει redirect τον χρήστη με ένα κωδικό σφάλματος */
+	/* Helper function that redirects back to index with an error number */
 	private function sendPostData($errorNumber) {
 		header("Location: index.php?error=" . $errorNumber);
 		exit();
 	}
 }
-
 $user = new User();
 $user->login();
 $user = NULL;
-
 ?>
--- challenges/izon/otp.php
+++ challenges/izon/otp.php
@@ -1,41 +1,38 @@
 <?php
-	/* ξεκινάμε το session για να έχουμε πρόσβαση στις μεταβλητές του */
+	/* Start the session in order to access its variables */
 	session_start();
-	/* αν ο χρήστης δεν έχει βάλει τίποτα στο πεδίο, τον ανακατευθύνουμε πίσω */
+	/* If the user left the otp field empty, we have to redirect him back with an error */
 	if (!isset($_POST['otp'])) {
 		header("Location: index.php?error=8");
-		
+		die();
 	}
-	/* Το OTP μας ισχύει για 180 δευτερόλεπτα απο την ώρα που θα πάρει τιμή. 
-	Παίρνουμε το timestamp απο την $_SESSION['ch04_stamp'] (η οποία ορίζεται στο index.php)
-	και ελέγχουμε αν δεν έχει "λήξει" το otp */
+	/* OTP is valid for 180 second (3 min) since initialized.
+	We check $_SESSION['ch04_stamp'] which contains the timestamp when the challenge initialized/reseted
+	against the current timestamp for expiration */
 	$stamp = $_SESSION['ch04_stamp'];
 	if ((time() - $_SESSION['ch04_stamp']) > 180) {
 		header ("Location: index.php?error=9");
-		
+		die();
 	}
-	/* Βρίσκουμε το username */
+	/* Calculate the username */
 	$username = hash('crc32', $_COOKIE['PHPSESSID']);
-	/* και κρατάμε τα πρώτα δύο ψηφία του */
+	/* and keep its first two characters */
 	$otp = substr($username, 0, 2);
-	/* Απο την ημερομηνία του timestamp (είναι η στιγμή που το $_SESSION['ch04_stamp']
-	παιρνει τιμή στο Index.php) βρίσκουμε την ώρα και την πολλαπλασιάζουμε με 4. Έπειτα 
-	σιγουρευόμαστε πως έχει πάντοτε  δύο ψηφία με την χρήση της sprinf */
+	/* Moreover, reconstruct the date from the stored timestamp, get the Hour it was 
+	calculated, multiply it by four (4) and make sure it always has two digits (04, 08 etc) */
 	$otp .= sprintf("%1$02d", (date("H", $_SESSION['ch04_stamp'])*4));
-	/* Επίσης απο το timestamp παίνρουμε τα λεπτά και προσθέτουμε 32.*/
+	/* Same goes for minutes, we get them from the stored timestamp and add 32 to the value */
 	$otp .= (date("i", $_SESSION['ch04_stamp'])+32);
-	/* Αν το otp που υπολογίστηκε είναι αυτό που έδωσε ο χρήστης, τότε στείλτον πίσω στο
-	Index, ενώ δίνουμε τιμές στο $_SESSION['ch04'] (είναι το flag για να ξέρουμε πως τελείωσε το challenge)
-	και στο αντίστοιχο timer για να δούμε πόση ώρα έκανε ο χρήστης για να το τελειώσει */
+	/* If the provided by the user OTP is the one calculated, then send him back to index, and 
+	set values to 'ch04' and 'ch04_timer' session variables (in order for the script to be aware
+	that the challenge is complete). */
 	if ($_POST['otp'] == $otp) {
 		$_SESSION['ch04']=1;
 		$_SESSION['ch04_timer']=time();
 		header("Location: index.php");
+		die();
 	} else {
 		header("Location: index.php?error=9");
+		die();
 	}
-		
-	
-	
-	
 ?>
