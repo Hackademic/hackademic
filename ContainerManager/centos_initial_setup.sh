fe6cb5b1a6a8125cc45b0cf48919879a74effefe
rauldeve <rahul.e.dev@gmail.com>
2015-01-22 18:44:23 -0800

    rename script files and bug fixes
    Signed-off-by: rauldeve <rahul.e.dev@gmail.com>

--- ContainerManager/centos_initial_setup.sh
+++ ContainerManager/centos_initial_setup.sh
@@ -2,7 +2,7 @@
 #script fpr setting up centos for container support
 
 yum update
-yum install libvirt virt-install epel
+yum install libvirt virt-install epel wget
 
 #steps to install rpmforge repository
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
@@ -11,7 +11,7 @@ wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.
 rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm
 
 yum clean all
-yum install httpd mysql mysql-server php php-mysql phpmyadmin union-fuse
+yum install httpd mysql mysql-server php php-mysql phpmyadmin unionfs-fuse
 
 service libvirtd restart
 chkconfig libvirtd on
@@ -20,4 +20,8 @@ service httpd restart
 service mysqld restart
 chkconfig httpd on
 chkconfig mysqld on
+
+#mysql add user and grant required privileges from sql file
+#download hackademic-next from github
+echo "Please run hackademic setup through the browser and restart the system before executing installer.py"
 
--- ContainerManager/container_hackademic_setup.sh
+++ ContainerManager/container_hackademic_setup.sh
@@ -2,6 +2,7 @@
 
 #change site root path
 sed -i 's&192.168.*/hackademic-next&'"$1"':'"$2"'/hackademic-next&' /var/www/html/hackademic-next/config.inc.php
+#add an entry for changing if its localhost
 
 #change location of mysql database
 sed -i 's/localhost/'"$1"'/' /var/www/html/hackademic-next/config.inc.php
