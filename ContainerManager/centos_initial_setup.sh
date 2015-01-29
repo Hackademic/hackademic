56bcf8b9dcbeed0cc130c043578d46fa25dac701
rauldeve <rahul.e.dev@gmail.com>
2015-01-28 18:34:18 -0800

    Small change to ensure proper mysql installation

--- ContainerManager/centos_initial_setup.sh
+++ ContainerManager/centos_initial_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#script fpr setting up centos for container support
+#script for setting up centos for container support
 
 yum update
 yum install libvirt virt-install epel wget
@@ -10,6 +10,7 @@ rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm
 
+#install necessary packages
 yum clean all
 yum install httpd mysql mysql-server php php-mysql phpmyadmin unionfs-fuse
 
@@ -21,7 +22,10 @@ service mysqld restart
 chkconfig httpd on
 chkconfig mysqld on
 
-#mysql add user and grant required privileges from sql file
+echo "Executing mysql_secure_installation. Please ensure that remote login is enabled"
+mysql_secure_installation
+
 #download hackademic-next from github
+wget https://github
 echo "Please run hackademic setup through the browser and restart the system before executing installer.py"
 
