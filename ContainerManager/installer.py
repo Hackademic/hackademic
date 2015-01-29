cdba86a6f391eeda8abe2a6fb815e4f12c51681a
rauldeve <rahul.e.dev@gmail.com>
2015-01-28 18:35:06 -0800

    change installer output format
    Signed-off-by: rauldeve <rahul.e.dev@gmail.com>

--- ContainerManager/installer.py
+++ ContainerManager/installer.py
@@ -4,15 +4,22 @@ __author__ = 'root'
 
 print "Hackademic sandbox installer"
 print "The installer assumes that you have read the README file"
-subprocess.call("./grant_priv.sh",shell=True)
+print"\n\n"
 
 
+mysql_password = raw_input("Enter the password used for mysql in hackademic setup\n")
+subprocess.call("./grant_priv.sh " + mysql_password,shell=True)
+
+#copy the files check.php and my trychallenges.php to hackademic-next installation
+print "\nPrepping hackademic-next installation for sandbox enviorment\n"
+subprocess.call("cp ../check.php /var/www/html/hackademic-next/check.php")
+subprocess.call("cp ../controller/class.TryChallengeController.php /var/www/html/hackademic-next/controller/class.TryChallengeController.php")
 config_string='#container manager configuration file'
 
-print "SElinux boolean has to be changed"
+print "Changing SElinux boolean. This will take some time.\n"
 subprocess.call("setsebool -P httpd_can_network_connect 1",shell=True)
 
-print 'Setting global configurations'
+print 'Setting up configurations file\n\n'
 
 #add global entry in config file
 config_string += '\n[global]\n'
@@ -27,7 +34,7 @@ config_string += 'ip address : ' + ip_address + '\n'
 
 #add container entries
 config_string += '\n[container]\n'
-default_ram_size = raw_input("Enter teh maximum amount of ram to be allocated to each container\n")
+default_ram_size = raw_input("Enter the maximum amount of ram to be allocated to each container\n")
 start_number = raw_input("Enter the number of containers to be started by default\n")
 
 config_string += 'master name : rootfs\n'
@@ -36,7 +43,7 @@ config_string += 'strat number : ' + start_number + '\n'
 
 #add port forwarding details
 config_string += '\n[port range]\n'
-port_start = raw_input("Enter the starting port number from which ports will be forwarded. Make sure subsequent ports does are not already allocated\n")
+port_start = raw_input("Enter the starting port number from which ports will be forwarded. Make sure subsequent ports does are not already allocated in MB\n")
 config_string += 'start : ' + port_start + '\n'
 
 print config_string
@@ -52,6 +59,7 @@ subprocess.call("mkdir " + container_root_path,shell=True)
 
 
 #print "The installer will now download the container image for CentOS 6.6. The best suport is a host operating system which is the same"
+#dosnt work. use as a user input?
 subprocess.call("wget http://images.linuxcontainers.org/images/centos/6/i386/default/20150114_02:16/rootfs.tar.gz",shell=True)
 
 
@@ -60,7 +68,7 @@ print "Extracting the first container"
 subprocess.call("tar -xvf rootfs.tar.xz -C " + container_root_path,shell=True)
 
 #execute container_hostname_setup.sh file as chroot
-subprocess.call("cp container_hostname_setup.sh " + container_root_path + "/container_hostname_setup.sh",shell=True)
+subprocess.call("mv container_hostname_setup.sh " + container_root_path + "/container_hostname_setup.sh",shell=True)
 subprocess.call("chroot " + container_root_path + " ./container_hostname_setup.sh " + 'rootfs',shell=True)
 
 #copy hackademic-next to container
