9736d2ea0aaf06dbf3603e508495c8b05ab920b3
rauldeve <rahul.e.dev@gmail.com>
2015-02-25 07:35:39 -0800

    add documentation for container_hostname_setu.sh
    Signed-off-by: rauldeve <rahul.e.dev@gmail.com>

--- ContainerManager/container_hostname_setup.sh
+++ ContainerManager/container_hostname_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+################################################################################################################################################################
+# This script is used to change the hostname of a container
+# It is necessary that every container have a unique hostname or else it will be difficult to extract the ip address assigned to it from the dnsleases file
+# By default every new container will have the hostname as LXC_NAME
+# This script is run brfore the installation of every new container to change its hostname
+################################################################################################################################################################
+
 #changes hostname of the container
 sed -i 's/LXC_NAME .*/'"$1"'/' /etc/hosts
 sed -i 's/HOSTNAME=.*/HOSTNAME='"$1"'/' /etc/sysconfig/network
