f2cf0697b9e4e735c16c0aa63379998e05eb65e0
rauldeve <rahul.e.dev@gmail.com>
2015-02-25 07:39:28 -0800

    add documentation for container_hackademic_setup.sh
    Signed-off-by: rauldeve <rahul.e.dev@gmail.com>

--- ContainerManager/container_hackademic_setup.sh
+++ ContainerManager/container_hackademic_setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+###############################################################################################################################################
+# This script is used to modify the hackademic installation inside the container
+# Since every container has its own ip address the config.inc.php files should be modified accordingly
+# The database installed in the main operating system is used as a central database for all the hackademic installations.
+# So the database entry in the config.inc.php file should also be changed
+################################################################################################################################################
+
 #change site root path
+#have to add entry to handle ip addresses other than those starting with those starting with 192.168
 sed -i 's&192.168.*/hackademic-next&'"$1"':'"$2"'/hackademic-next&' /var/www/html/hackademic-next/config.inc.php
 #add an entry for changing if its localhost
 
