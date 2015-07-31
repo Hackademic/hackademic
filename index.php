62f542e4cc47bd3affbdc25e582050a69f8db76b
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-07-31 07:17:07 +0530

    Base folder fixed

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 /**
- *
  * Hackademic-CMS/index.php
  * This file is the entry point for the application
  *
@@ -8,25 +7,27 @@
  *
  * LICENSE:
  *
- * This file is part of Hackademic CMS (https://www.owasp.org/index.php/OWASP_Hackademic_Challenges_Project).
+ * This file is part of Hackademic CMS
+ * (https://www.owasp.org/index.php/OWASP_Hackademic_Challenges_Project).
  *
- * Hackademic CMS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
- * later version.
+ * Hackademic CMS is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
  *
- * Hackademic CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
+ * Hackademic CMS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Hackademic CMS.  If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * Hackademic CMS.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * PHP Version 5
  *
- * @author Pragya Gupta <pragya18nsit[at]gmail[dot]com>
- * @author Konstantinos Papapanagiotou <conpap[at]gmail[dot]com>
- * @license http://www.gnu.org/licenses/gpl.html
+ * @author    Pragya Gupta <pragya18nsit@gmail.com>
+ * @author    Konstantinos Papapanagiotou <conpap@gmail.com>
  * @copyright 2012 OWASP
- *
+ * @license   GNU General Public License http://www.gnu.org/licenses/gpl.html
  */
 $version = explode('.', PHP_VERSION);
 if ($version[0] < 5) {
@@ -36,18 +37,18 @@ if ($version[0] < 5) {
 }
 if (!file_exists('config.inc.php')) {
     header("Location: ./installation/install.php");
-	error_log("Couldn't find config file, installing");
+    error_log("Couldn't find config file, installing");
     die("No config");
 }
-require_once('init.php');
+require_once 'init.php';
 
 $url = isset($_GET['url']) ? $_GET['url'] : '';
-if($url != '') {
-  require_once(HACKADEMIC_PATH . "model/common/class.Page.php");
-  $path = Page::getFile($url);
-  require_once(HACKADEMIC_PATH . $path['file']);
-} else { 
-  require_once(HACKADEMIC_PATH . "controller/class.LandingPageController.php");
-  $controller = new LandingPageController();
-  echo $controller->go();
+if ($url != '') {
+    require_once HACKADEMIC_PATH."model/common/class.Page.php";
+    $path = Page::getFile($url);
+    include_once HACKADEMIC_PATH.$path['file'];
+} else {
+    require_once HACKADEMIC_PATH."controller/class.LandingPageController.php";
+    $controller = new LandingPageController();
+    echo $controller->go();
 }
--- init.php
+++ init.php
@@ -1,36 +1,38 @@
 <?php
 /**
- *
  * Hackademic-CMS/init.php
  *
  * Copyright (c) 2012 OWASP
  *
  * LICENSE:
  *
- * This file is part of Hackademic CMS (https://www.owasp.org/index.php/OWASP_Hackademic_Challenges_Project).
+ * This file is part of Hackademic CMS
+ * (https://www.owasp.org/index.php/OWASP_Hackademic_Challenges_Project).
  *
- * Hackademic CMS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
- * later version.
+ * Hackademic CMS is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
  *
- * Hackademic CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * Hackademic CMS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with Hackademic CMS.  If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * Hackademic CMS.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * PHP Version 5
  *
- * @author Pragya Gupta <pragya18nsit[at]gmail[dot]com>
- * @author Konstantinos Papapanagiotou <conpap[at]gmail[dot]com>
- * @license http://www.gnu.org/licenses/gpl.html
+ * @author    Pragya Gupta <pragya18nsit@gmail.com>
+ * @author    Konstantinos Papapanagiotou <conpap@gmail.com>
  * @copyright 2012 OWASP
- *
+ * @license   GNU General Public License http://www.gnu.org/licenses/gpl.html
  */
 
-require_once("config.inc.php");
-require_once("model/common/class.Loader.php");
-require_once("model/common/class.HackademicDB.php");
+require_once "config.inc.php";
+require_once "model/common/class.Loader.php";
+require_once "model/common/class.HackademicDB.php";
 
 if (defined('ENVIRONMENT') && ENVIRONMENT == "dev") {
     ini_set('display_errors', true);
@@ -42,21 +44,28 @@ $db = new HackademicDB();
 Loader::init();
 
 
-require_once("esapi/class.Esapi_Utils.php");
+require_once "esapi/class.Esapi_Utils.php";
 
-if(!isset($ESAPI_utils)){
-			// error_log("Esapi not inited in init", 0);
-			$ESAPI_utils = new Esapi_Utils();
+if (!isset($ESAPI_utils)) {
+    // error_log("Esapi not inited in init", 0);
+    $ESAPI_utils = new Esapi_Utils();
 }
 
-function removeDirectory($path) {
-		$files = glob($path . '/*');
-		foreach ($files as $file) {
-			is_dir($file) ? removeDirectory($file) : unlink($file);
-		}
-		rmdir($path);
-		return;
-	}
-if(file_exists (HACKADEMIC_PATH."/installation") && defined('ENVIRONMENT') && ENVIRONMENT != "dev")
-	removeDirectory(HACKADEMIC_PATH."/installation");
+/**
+ * Function to remove directory.
+ * @param  path $path Path of the directory to be removed.
+ * @return control
+ */
+function removeDirectory($path)
+{
+    $files = glob($path . '/*');
+    foreach ($files as $file) {
+        is_dir($file) ? removeDirectory($file) : unlink($file);
+    }
+    rmdir($path);
+    return;
+}
+if (file_exists(HACKADEMIC_PATH."/installation") && defined('ENVIRONMENT') && ENVIRONMENT != "dev") {
+    removeDirectory(HACKADEMIC_PATH."/installation");
+}
 ?>
